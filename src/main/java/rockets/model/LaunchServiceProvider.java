7dc61cef147e179692125bbdd2123af5717d5201
namdaisu <48665808+namdaisu@users.noreply.github.com>
2019-04-26 03:21:31 +1000

    Update LaunchServiceProvider.java

--- src/main/java/rockets/model/LaunchServiceProvider.java
+++ src/main/java/rockets/model/LaunchServiceProvider.java
@@ -3,11 +3,12 @@ package rockets.model;
 import com.google.common.collect.Sets;
 
 import java.math.BigDecimal;
-import java.util.HashSet;
 import java.util.Objects;
 import java.util.Set;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
-import static rockets.model.Launch.LaunchOutcome.FAILED;
+import static org.apache.commons.lang3.Validate.*;
 import static rockets.model.Launch.LaunchOutcome.SUCCESSFUL;
 
 public class LaunchServiceProvider extends Entity {
@@ -21,66 +22,49 @@ public class LaunchServiceProvider extends Entity {
 
     private Set<Rocket> rockets;
 
-    private Set<Launch> launchs;
+
 
     //cc new
+    //private Set<Launch> launches;
     private BigDecimal totalRevenue;
-   private int percentage;
-   private int Dominant;
+    private int percentage;
+    private int Dominant;
+
+    public boolean onlyCharacter(String input) {
+        return input.matches("[a-zA-Z\\s]+");
+    }
+
+    public boolean isInteger(String input){
+        return input.matches("[0-9]+");
+    }
+
+    public boolean isInRange(String input, int i) {return input.length()<i;}
+
+    public boolean notFutureYear(int year) {
+        Date date = new Date();
+        String strDateFormat = "yyyy";
+        SimpleDateFormat sdf = new SimpleDateFormat(strDateFormat);
+        //System.out.println(sdf);
+        if(year<Integer.valueOf(sdf.format(date))) return true;
+        else return false;
+    }
 
     public LaunchServiceProvider(String name, int yearFounded, String country) {
-        this.name = name;
-        this.yearFounded = yearFounded;
-        this.country = country;
 
-        rockets = Sets.newLinkedHashSet();// new
-    }
+        if(onlyCharacter(name)
+                && onlyCharacter(country)
+                && isInteger(String.valueOf(yearFounded))
+                && notFutureYear(yearFounded)
+                && isInRange(name,30)
+                && isInRange(country, 30)){
+            this.name = name;
+            this.yearFounded = yearFounded;
+            this.country = country;
 
-    //new
-    public BigDecimal getTotalRevenue(int year) {
-        BigDecimal totalRevenue = new BigDecimal(0.00);
-        Set<Launch> set = getLaunchs();
-        for (Launch st : set) {
-            if (st.getLaunchDate().getYear()==year)
-            {
-                totalRevenue.add(st.getPrice());
-            }
+            rockets = Sets.newLinkedHashSet();
         }
-        return totalRevenue;
-    }
+        else {throw new IllegalArgumentException("Year must be an integer, name and country must only contain character, less than 30 in length");}
 
-    public int getPercentage() {
-        Set<Launch> set = getLaunchs();
-        int succ=0;
-        for (Launch st : set) {
-            if (st.getLaunchOutcome()==SUCCESSFUL)
-            {
-                succ+=1;
-            }
-        }
-        percentage = succ/getLaunchs().size();
-        return percentage;
-    }
-
-    public int getDominant(String orbit) {
-        Set<Launch> set = getLaunchs();
-        int Dominant =0;
-        for (Launch st : set) {
-            if (st.getOrbit()==orbit)
-            {
-                Dominant+=1;
-            }
-        }
-
-        return Dominant;
-    }
-
-    public Set<Launch> getLaunchs() {
-        return launchs;
-    }
-
-    public void setLaunchs(Set<Launch> launchs) {
-        this.launchs = launchs;
     }
 
     public String getName() {
@@ -99,15 +83,24 @@ public class LaunchServiceProvider extends Entity {
         return headquarters;
     }
 
-    public Set<Rocket> getRockets() {
-        return rockets;
-    }
+
+    public Set<Rocket> getRockets() { return rockets; }
+
+
 
     public void setHeadquarters(String headquarters) {
-        this.headquarters = headquarters;
+
+        //ADDED
+        notBlank(headquarters, "headquarter cannot be null or empty" );
+        if (isInRange(headquarters, 30)) {
+            this.headquarters = headquarters;
+        }
+        else throw new IllegalArgumentException("headquarter must be under 30 characters");
+
     }
 
     public void setRockets(Set<Rocket> rockets) {
+        notNull(rockets, "rockets cannot be null or empty" );
         this.rockets = rockets;
     }
 
@@ -123,7 +116,71 @@ public class LaunchServiceProvider extends Entity {
 
     @Override
     public int hashCode() {
-
         return Objects.hash(name, yearFounded, country);
     }
+
+
+
+    //new
+    public BigDecimal getTotalRevenue(int year) {
+        BigDecimal totalRevenue = new BigDecimal(  0.00);
+        Set<Rocket> set = getRockets();
+        for (Rocket ro :set){
+              Set<Launch> set1 =ro.getLaunches();
+            for (Launch st : set1) {
+                if (st.getLaunchDate().getYear() == year) {
+                    totalRevenue.add(st.getPrice());
+                }
+            }
+        }
+        return totalRevenue;
+    }
+
+    public int getPercentage() {
+        int succ=0;
+        int size =0;
+        Set<Rocket> set = getRockets();
+        for (Rocket ro :set)
+        {
+            Set<Launch> set1 = ro.getLaunches();
+            size+=set1.size();
+            for (Launch st : set1)
+            {
+                //size+=1;
+                if (st.getLaunchOutcome() == SUCCESSFUL)
+                {
+                    succ += 1;
+                }
+            }
+        }
+
+        if (size>0)
+        {
+            percentage = succ / size;
+        }
+        //System.out.println(percentage);
+        return percentage;
+    }
+
+
+
+    public int getDominant(String orbit) {
+        Set<Rocket> set = getRockets();
+        int Dominant =0;
+        for (Rocket ro :set)
+        {
+            Set<Launch> set1 = ro.getLaunches();
+
+            for (Launch st : set1) {
+                //System.out.println(st.getOrbit());
+                if (st.getOrbit() == orbit) {
+                    Dominant += 1;
+                }
+            }
+        }
+        System.out.println(Dominant);
+        return Dominant;
+    }
+
+
 }
