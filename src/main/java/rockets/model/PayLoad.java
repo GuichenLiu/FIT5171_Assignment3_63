f0f98390699b1ecb3a17c1dc286682a49498f823
tungnam <thoo0013@student.monash.edu>
2019-04-26 19:33:57 +1000

    first commit

---
+++ src/main/java/rockets/model/PayLoad.java
@@ -0,0 +1,94 @@
+package rockets.model;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Objects;
+import static org.apache.commons.lang3.Validate.notBlank;
+import static org.apache.commons.lang3.Validate.notNull;
+
+public class PayLoad {
+
+    //satellite, space probe, spacecraft
+    private String type;
+    //payload name
+    private String name;
+    //weight of payload
+    private String mass;
+
+    private String manufacturer;
+
+    public boolean onlyCharacter(String input) {
+        return input.matches("[a-zA-Z\\s]+");
+    }
+
+    public boolean onlyCharacterAndNumber(String input) {return input.matches("[a-zA-Z0-9\\s]+"); }
+
+    public boolean isInRange(String input, int i) {return input.length()<i;}
+
+    public PayLoad(String type, String name, String manufacturer) {
+        if(onlyCharacter(type)
+                &&onlyCharacterAndNumber(name)
+                &&onlyCharacter(manufacturer)
+                &&isInRange(type,30)
+                &&isInRange(name,30)
+                &&isInRange(manufacturer,100)
+        ){
+            this.type = type;
+            this.name = name;
+            this.manufacturer = manufacturer;
+        }
+        else throw new IllegalArgumentException("Type and Manufacturer conctain only alphabetic, Name contain only alphanumeric, all are under 30 characters");
+    }
+
+    public String getType(){return type;}
+
+    public String getName(){return name;}
+
+    public String getMass(){return mass; }
+
+    public String getManufacturer(){return manufacturer;}
+
+    public boolean isDoubleOrInteger(String input) {
+
+        if (input.matches(("(([0-9]*)\\.([0-9]*))")) || input.matches(("[0-9]+"))) return true;
+        else return false;
+    }
+
+
+    public void setMass(String weight, String unit){
+        notBlank(weight, "weight and unit cannot be null or empty");
+        notBlank(unit, "weight and unit cannot be null or empty");
+
+        if(isDoubleOrInteger(weight) && onlyCharacter(unit))
+        {
+            this.mass = weight + " " + unit;
+        }
+        else throw new IllegalArgumentException("Weight must be Integer or Double, unit contain only characters");
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        PayLoad payload = (PayLoad) o;
+        return Objects.equals(type, payload.type) &&
+                Objects.equals(name, payload.name) &&
+                Objects.equals(manufacturer, payload.manufacturer);
+
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(type, name, manufacturer);
+    }
+
+    @Override
+    public String toString() {
+        return "PayLoad{" +
+                "type='" + type + '\'' +
+                "name='" + name + '\'' +
+                "manufacturer='" + manufacturer + '\'' +
+                '}';
+    }
+
+}
---
+++ src/main/java/rockets/model/Rocket.java
@@ -0,0 +1,201 @@
+package rockets.model;
+
+import java.util.Objects;
+import java.util.Set;
+
+import static org.apache.commons.lang3.Validate.notBlank;
+import static org.apache.commons.lang3.Validate.notNull;
+
+public class Rocket extends Entity {
+
+
+    private Set<PayLoad> payloads;
+    private String name;
+    private String variation;
+    private String country;
+
+    private LaunchServiceProvider manufacturer;
+
+    private String massToLEO;
+
+    private String massToGTO;
+
+    private String massToOther;
+
+    public Set<PayLoad> getPayloads() {
+        return payloads;
+    }
+
+    public void setPayloads(Set<PayLoad> payloads) {
+        this.payloads = payloads;
+    }
+
+    // cc new
+    private Set<Launch> launches;
+
+    public Set<Launch> getLaunches() {
+        return launches;
+    }
+
+    public void setLaunches(Set<Launch> launches) {
+        this.launches = launches;
+    }
+
+    /**
+     * All parameters shouldn't be null.
+
+     * @param name
+     * @param country
+     * @param manufacturer
+     */
+    public Rocket(String name,String variation, String country,LaunchServiceProvider  manufacturer) {
+        notNull(name);
+        notNull(country);
+        notNull(manufacturer);
+        //
+        if(
+                haveCharaters(name)
+                        &&onlyCharacter(country)
+                        //&&onlyCharacter(manufacturer)
+                        &&isInRange(name,30)
+                        &&isInRange(variation,30)
+                        &&isInRange(country,30)
+            //&&isInRange(manufacturer,30)
+        )
+        {
+            this.name = name;
+            this.variation = variation;
+            this.country = country;
+            this.manufacturer = manufacturer;
+        }
+        else {
+            throw new IllegalArgumentException("\"name should under 30 characters\\n and contains alphabetic characters\" +\n" +
+                    "                    \" variation should under 30 characters\"+\n" +
+                    "                    \" country should under 30 characters, contain only alphabetic characters \"+\n" +
+                    "                    \"manufacturer should under 30 characters, contain only alphabetic characters\"");
+        }
+    }
+
+    public boolean haveCharaters(String name) {
+        return name.matches(".*[a-zA-z].*");
+
+    }
+
+
+
+
+
+    public boolean isInRange(String input, int i) {
+        return input.length()<=i;
+    }
+
+    public boolean onlyCharacter(String input) {
+        return input.matches("[a-zA-Z]+");
+    }
+
+
+    public boolean isInteger(String input){
+        return input.matches("[0-9]+");
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getVariation() {
+        return variation;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+
+    public LaunchServiceProvider getManufacturer() {
+
+        return manufacturer;
+    }
+
+    public String getMassToLEO() {
+        return massToLEO;
+    }
+
+    public String getMassToGTO() {
+        return massToGTO;
+    }
+
+    public String getMassToOther() {
+        return massToOther;
+    }
+
+    public boolean LEOGTOOTHERIsValid(String input) {
+
+        //whether  contains
+        return input.matches(".*[0-9].*") ;
+
+    }
+
+
+    // cici edited
+    public void setMassToLEO(String massToLEO) {
+        notBlank(massToLEO, "MassToLEO cannot be null or empty");
+        if ((LEOGTOOTHERIsValid(massToLEO) == false)) {
+            throw new IllegalArgumentException("MassToLEO should contain numbers");
+        }
+        else{
+            this.massToLEO = massToLEO;
+        }
+
+    }
+
+    // cici edited
+    public void setMassToGTO(String massToGTO) {
+        notBlank(massToGTO, "MassToGTO cannot be null or empty");
+        if ((LEOGTOOTHERIsValid(massToGTO) == false)) {
+            throw new IllegalArgumentException("MassToGTO should contain numbers");
+        }
+        else {
+            this.massToGTO = massToGTO;
+        }
+    }
+
+    // cici edited
+    public void setMassToOther(String massToOther) {
+        notBlank(massToOther, "MassToOther cannot be null or empty");
+        if ((LEOGTOOTHERIsValid(massToOther) == false)) {
+            throw new IllegalArgumentException("MassToOther should contain numbers");
+        }
+        else {
+            this.massToOther = massToOther;
+        }
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Rocket rocket = (Rocket) o;
+        return Objects.equals(name, rocket.name) &&
+                Objects.equals(variation, rocket.variation) &&
+                Objects.equals(country, rocket.country) &&
+                Objects.equals(manufacturer, rocket.manufacturer);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name,variation, country, manufacturer);
+    }
+
+    @Override
+    public String toString() {
+        return "Rocket{" +
+                "name='" + name + '\'' +
+                ", variation='" + variation + '\'' +
+                ", country='" + country + '\'' +
+                ", manufacturer='" + manufacturer + '\'' +
+                ", massToLEO='" + massToLEO + '\'' +
+                ", massToGTO='" + massToGTO + '\'' +
+                ", massToOther='" + massToOther + '\'' +
+                '}';
+    }
+}
