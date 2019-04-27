00a2ba9be7ea0b8ccab0a96d82dfb04e42acce97
tungnam <thoo0013@student.monash.edu>
2019-04-27 18:41:54 +1000

    change2704

--- src/test/java/rockets/model/PayLoadUnitTest.java
+++ src/test/java/rockets/model/PayLoadUnitTest.java
@@ -11,30 +11,41 @@ import org.junit.jupiter.params.provider.ValueSource;
 
 class PayLoadUnitTest {
 
-    private PayLoad target;
+    //private PayLoad target;
 
     @BeforeEach
     public void setUp() {
-        String type = "Satellite";
-        String name = "Sputnik I";
-        String mass = "83.6 kg";
-        //String manufacturer = "Ministry of Radiotechnical Industry";
-        LaunchServiceProvider  manufacturer = new LaunchServiceProvider("SpaceX", 2002, "USA");
-        target = new PayLoad(type,name,manufacturer);
     }
+
+
+//    @DisplayName("should create payload successfully when given right parameters to constructor")
+//    @Test
+//    public void shouldConstructRocketObject() {
+//        String type = "Satellite";
+//        String name = "Vanguard 1";
+//        String mass = "1.47 kg";
+//        LaunchServiceProvider  manufacturer = new LaunchServiceProvider("SpaceX", 2002, "USA");
+//        Rocket bfr = new Rocket(name,variation,country,manufacturer);
+//        assertNotNull(bfr);
+//    }
 
     @DisplayName("should return true when pass the valid parameters for constructor")
     @Test
     public void shouldReturnTrueWhenPassValidInputToConstructor() {
+        String type = "satellite";
+        String name = "sputnik 1";
+        LaunchServiceProvider  manufacturer = new LaunchServiceProvider("OKB", 1957, "Russia");
+        PayLoad payload = new PayLoad(type,name,manufacturer);
+        assertNotNull(payload);
+    }
+
+    @DisplayName("should throw exception when given null manufacturer to constructor")
+    @Test
+    public void shouldThrowExceptionWhenNoManufacturerGiven() {
         String type = "Satellite";
         String name = "Vanguard 1";
-        String mass = "1.47 kg";
-        //String manufacturer = "Naval Research Laboratory";
-        LaunchServiceProvider  manufacturer = new LaunchServiceProvider("SpaceX", 2002, "USA");
-        target = new PayLoad(type,name,manufacturer);
-        assertTrue(type.equals(target.getType()));
-        assertTrue(name.equals(target.getName()));
-        assertTrue(manufacturer.equals(target.getManufacturer()));
+        //String mass = "1.47 kg";
+        assertThrows(NullPointerException.class, () -> new PayLoad(type, name, null));
     }
 
 
@@ -59,14 +70,17 @@ class PayLoadUnitTest {
     @DisplayName("should throw exception when pass empty weight and unit to setMass function")
     @Test
     public void shouldThrowExceptionWhenSetWeightAndUnitToNull() {
-
-        NullPointerException exception = assertThrows(NullPointerException.class, () -> target.setMass(null,null));
+        String type = "satellite";
+        String name = "sputnik 1";
+        LaunchServiceProvider  manufacturer = new LaunchServiceProvider("OKB", 1957, "Russia");
+        PayLoad payload = new PayLoad(type,name,manufacturer);
+        NullPointerException exception = assertThrows(NullPointerException.class, () -> payload.setMass(null,null));
         assertEquals("weight and unit cannot be null or empty", exception.getMessage());
 
-        NullPointerException exception1 = assertThrows(NullPointerException.class, () -> target.setMass("1.2",null));
+        NullPointerException exception1 = assertThrows(NullPointerException.class, () -> payload.setMass("1.2",null));
         assertEquals("weight and unit cannot be null or empty", exception.getMessage());
 
-        NullPointerException exception2 = assertThrows(NullPointerException.class, () -> target.setMass(null,"kg"));
+        NullPointerException exception2 = assertThrows(NullPointerException.class, () -> payload.setMass(null,"kg"));
         assertEquals("weight and unit cannot be null or empty", exception.getMessage());
     }
 
@@ -75,15 +89,18 @@ class PayLoadUnitTest {
     @DisplayName("should throw exception when pass invalid weight and unit to setMass function")
     @Test
     public void shouldThrowExceptionWhenPassInvalidWeightAndUnitToSetMass(){
-
-        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> target.setMass("aaa", "kg"));
+        String type = "satellite";
+        String name = "sputnik 1";
+        LaunchServiceProvider  manufacturer = new LaunchServiceProvider("OKB", 1957, "Russia");
+        PayLoad payload = new PayLoad(type,name,manufacturer);
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> payload.setMass("aaa", "kg"));
         assertEquals("Weight must be Integer or Double, unit contain only characters", exception.getMessage());
 
-        IllegalArgumentException exception1 = assertThrows(IllegalArgumentException.class, () -> target.setMass("1.2","!!!"));
+        IllegalArgumentException exception1 = assertThrows(IllegalArgumentException.class, () -> payload.setMass("1.2","!!!"));
         assertEquals("Weight must be Integer or Double, unit contain only characters", exception1.getMessage());
 
-        target.setMass("1.2", "kg");
-        assertEquals("1.2 kg",target.getMass());
+        payload.setMass("1.2", "kg");
+        assertEquals("1.2 kg",payload.getMass());
     }
 
 }
